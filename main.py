ed1c28aebfe8e7ab965803de37b22d0fee60bd9b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-06-26 12:20:58 +0600

    feat: implement decorators

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
-def read_file(file_path) -> list[str]:
+from collections.abc import Generator
+import logging
+import time
+from functools import wraps
+
+def timing(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} took {end - start:.6f}s")
+        return result
+    return wrapper
+
+def logger(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"calling {func.__name__} with {args} {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned {result}")
+        return result
+    return wrapper
+
+@timing
+@logger
+def read_file(file_path: str) -> Generator[str, None, None]:
     try:
         with open(file=file_path, mode="r", encoding='utf-8') as file:
-            return file.readlines()
-    except Exception as e:
-        print(f"Error: {e}")
-        return []
+            for line in file:
+                yield line.rstrip("\n")
+    except FileNotFoundError as f:
+        logging.error(f"File not found: {f}")
+        raise
+    except OSError as os:
+        logging.error(f"OS error while reading file: {os}")
+        raise
+
+
 
 def main():
     FILE = "file.txt"
-    print(read_file(FILE))
-
+    gen = read_file(FILE)
+    
+    for line in gen:
+        print(line)
+    
 
 if __name__ == "__main__":
     main()
